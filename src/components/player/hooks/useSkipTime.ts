47048c925e895762a6a30e9254a8a26dba7dbcd5
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-02-13 12:33:04 -0700

    update creds

--- src/components/player/hooks/useSkipTime.ts
+++ src/components/player/hooks/useSkipTime.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { usePlayerMeta } from "@/components/player/hooks/usePlayerMeta";
 
-// Thanks Nemo, Custom, and Roomba for this API
+// Thanks Nemo for this API
 const BASE_URL = "https://fed-api-production.up.railway.app";
 
 export function useSkipTime() {
