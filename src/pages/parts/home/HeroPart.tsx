2faa7eb99d34b9d0bf31b36e38fff0de8e5df9f9
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-15 21:59:50 -0700

    init beta

--- src/pages/parts/home/HeroPart.tsx
+++ src/pages/parts/home/HeroPart.tsx
@@ -106,7 +106,7 @@ export function HeroPart({ setIsSticky, searchParams }: HeroPartProps) {
       <div className="mt-44 space-y-16 text-center">
         <div className="relative z-10 mb-16">
           {isTV && search.length > 0 ? null : (
-            <HeroTitle className="mx-auto max-w-md">{title}</HeroTitle>
+            <HeroTitle>P-Stream Beta</HeroTitle>
           )}
         </div>
         <div className="relative h-20 z-30">
