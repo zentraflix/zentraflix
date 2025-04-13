a026ed4352b1d9186fe85ba0b82e0dfbe48ef917
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-04-13 11:14:58 -0600

    Update base.ts

--- src/components/player/display/base.ts
+++ src/components/player/display/base.ts
@@ -35,6 +35,7 @@ const levelConversionMap: Record<number, SourceQuality> = {
   1080: "1080",
   720: "720",
   480: "480",
+  2160: "4k",
 };
 
 function hlsLevelToQuality(level?: Level): SourceQuality | null {
