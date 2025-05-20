077edd7da805e1d44e6129ac97435f0324ff9fba
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-05-20 14:05:43 -0600

    Update IosPwaLimitations.tsx

--- src/components/buttons/IosPwaLimitations.tsx
+++ src/components/buttons/IosPwaLimitations.tsx
@@ -4,7 +4,7 @@ import { Icon, Icons } from "../Icon";
 function IosPwaLimitations() {
   const showAlert = () => {
     alert(
-      "Due to Apple’s limitations, Picture-in-Picture (PiP) and Fullscreen are disabled on iOS PWAs. Use the browser vertion to re-enable these features.\n" +
+      "Due to Apple’s limitations, Picture-in-Picture (PiP) and Fullscreen are disabled on iOS PWAs. Use the browser version to re-enable these features.\n" +
         "Tip: To hide the iOS home indicator, use guided access within the PWA!",
     );
   };
