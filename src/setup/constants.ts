dd863393d76803849cf05c2191483b6941642ccf
vicky0604hello <choudharyji0604e@gmail.com>
2025-07-21 09:13:29 +0530

    ZentraFlix

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/7z6znYgrTG";
+export const DISCORD_LINK = "https://t.me/ZentraFlix";
 // export const GITHUB_LINK = "https://github.com/ZentraFlix/ZentraFlix";
 export const TWITTER_LINK = "https://twitter.com/SudoFlix";
 export const GA_ID = import.meta.env.VITE_GA_ID;
